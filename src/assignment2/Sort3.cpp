15aa871d560a223e127fdf43f1a21e9c2f83149e
NoMad312 <32452162+NoMad312@users.noreply.github.com>
2019-10-16 23:36:07 +0900

    Update Sort3.cpp

--- src/assignment2/Sort3.cpp
+++ src/assignment2/Sort3.cpp
@@ -3,8 +3,22 @@
 
 using namespace std;
 
-void Sort3(int& i, int& j, int& k)
+void Swap(int& variable1, int& variable2)
 {
+    int temp;
+    temp = variable1;
+    variable1 = variable2;
+    variable2 = temp;
+}
+
+void Sort3(int& i, int& j, int& k)
+{   if i > j & i > k:
+        if k > j: Swap(k,j);
+    if j > i & j > k:Swap(i,j)
+        if k > j: Swap(k,j);
+    if k > j & k > i:Swap(k,i)
+        if k > j: Swap(k,j);
+    
     // TODO: fill in the body.
     // You MUST use the Swap function located in Sort3.hpp.
 }
